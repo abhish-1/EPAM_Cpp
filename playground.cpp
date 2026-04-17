5b4b9e9abfe1c9b763065b39e8317496e33526b3
Abhishek Kumar <abhishekkant135@gmail.com>
2026-04-17 21:01:45 +0530

    Update playground.cpp to implement multi-threading example with counter increment; remove unused code and add necessary includes

--- playground.cpp
+++ playground.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include <memory>
-#include<typeinfo>
-using namespace std;
+#include <thread>
+#include <vector>
+#include <mutex>
 
-void getDataonPointer(const unique_ptr<int>& ptr){
-    cout<<typeid(ptr).name()<<endl;
-    cout<<ptr.get()<<endl;
-    cout<<*ptr<<endl;
-    cout<<&ptr<<endl;
+int counter = 0;
+std::mutex m;
+
+void work() {
+    for (int i = 0; i < 1000; i++) {
+        std::lock_guard<std::mutex> lock(m);
+        counter++; // protected
+    }
 }
 
-
 int main() {
-    unique_ptr<int> ptr(new int(10));
-    getDataonPointer(ptr);
-    unique_ptr<int> ptrCopy = move(ptr);
-    //getDataonPointer(ptr);
-    cout<<endl<<endl;
-    getDataonPointer(ptrCopy);
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < 5; i++) {
+        threads.emplace_back(work);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "Final counter = " << counter << std::endl;
+    std::cout << "Expected = 5000" << std::endl;
+
+    return 0;
 }
